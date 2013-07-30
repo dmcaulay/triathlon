7177dba8f2182af9322f778fa12fdc2c14c44bb1
Dan McAulay <dmcaulay@gmail.com>
2013-07-29 18:51:18 -0700

    adding secondary indexes to the riak wrapper

--- lib/riak.ex
+++ lib/riak.ex
@@ -1,9 +1,10 @@
 
 defmodule Riak do
-  defrecord Bucket, pid: nil, name: nil
+  defrecord Bucket, pid: nil, name: nil, indexes: nil
+  defrecord Index, type: nil, field: nil
 
   def connect(host, port) do
-    :riakc_pb_socket.start_link(host, port)
+    :riakc_pb_socket.start_link host, port
   end
 
   def create_bucket(host, port, name) do
@@ -18,8 +19,13 @@ defmodule Riak do
     {:ok, Bucket.new pid: pid, name: name}
   end
 
+  def set_indexes(bucket, indexes) do
+    bucket.indexes indexes
+  end
+
   def set(bucket, key, obj) do
     riak_obj = :riakc_obj.new bucket.name, key, obj
+    riak_obj = add_indexes bucket, obj, riak_obj
     :riakc_pb_socket.put bucket.pid, riak_obj
   end
 
@@ -32,4 +38,39 @@ defmodule Riak do
       _ -> res
     end
   end
+
+  def delete(bucket, key) do
+    :riakc_pb_socket.delete bucket.pid, bucket.name, key
+  end
+
+  def find(bucket, {field, value}) do
+    res = :riakc_pb_socket.get_index(bucket.pid, bucket.name, {:binary_index, field}, value)
+    case res do
+      {:ok, {_,keys,_,_}} -> 
+        Enum.map keys, get(bucket, &1)
+      _ -> res
+    end
+  end
+
+  def find(bucket, {field, start, stop}) do
+    res = :riakc_pb_socket.get_index(bucket.pid, bucket.name, {:integer_index, field}, start, stop)
+    case res do
+      {:ok, keys} -> 
+        Enum.map keys, get(bucket, &1)
+      _ -> res
+    end
+  end
+
+  defp add_indexes(Bucket[indexes: nil], _, riak_obj), do: riak_obj
+
+  defp add_indexes(bucket, obj, riak_obj) do
+    meta = :riakc_obj.get_update_metadata riak_obj
+    to_index = Enum.map bucket.indexes, fn(i = {type, field}) ->
+      {val, _} = Code.eval_string "obj.#{field}", [obj: obj]
+      {i, [val]}
+    end
+    indexed = :riakc_obj.set_secondary_index meta, to_index
+    :riakc_obj.update_metadata riak_obj, indexed
+  end
+
 end
--- test/riak_test.exs
+++ test/riak_test.exs
@@ -25,4 +25,25 @@ defmodule RiakTest do
     assert Riak.get(bucket, "test_data") == to_save
   end
 
+  test "deleting an object in the db" do
+    {:ok, bucket} = Riak.create_bucket('127.0.0.1', 8087, "test")
+    to_save = TestObject.new name: "test", data: "this is fun"
+    assert Riak.set(bucket, "test_data", to_save) == :ok
+    assert Riak.get(bucket, "test_data") == to_save
+    assert Riak.delete(bucket, "test_data") == :ok
+    assert Riak.get(bucket, "test_data") == {:error,:notfound}
+  end
+
+  test "adding secondary indexes on a bucket" do
+    {:ok, bucket} = Riak.create_bucket('127.0.0.1', 8087, "test")
+    bucket = Riak.set_indexes bucket, binary_index: "name"
+    to_save = TestObject.new name: "test", data: "this is fun"
+    assert Riak.set(bucket, "test_data", to_save) == :ok
+    to_save_2 = TestObject.new name: "not_test", data: "this is not fun"
+    assert Riak.set(bucket, "test_data_2", to_save_2) == :ok
+    to_save_3 = TestObject.new name: "test", data: "this is amazing"
+    assert Riak.set(bucket, "test_data_3", to_save_3) == :ok
+    assert Riak.find(bucket, {'name', "test"}) == [to_save_3,to_save]
+  end
+
 end
